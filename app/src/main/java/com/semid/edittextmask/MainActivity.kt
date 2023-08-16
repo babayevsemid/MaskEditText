35743113673ac26e6ae755ec2cd6eb776fc138f9
Samid Babayev <samidbabayev@Samids-MacBook-Pro.local>
2023-08-16 04:07:58 -0700

    Minor changes

--- app/src/main/java/com/semid/edittextmask/MainActivity.kt
+++ app/src/main/java/com/semid/edittextmask/MainActivity.kt
@@ -2,8 +2,6 @@ package com.semid.edittextmask
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.semid.maskedittext.MaskUtil
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
